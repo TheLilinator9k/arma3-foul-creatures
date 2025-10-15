2cfa9dbc1d9734ad5cddd3bdef335c33332c1208
TheLilinator9k <duck_amuck@hotmail.com>
2025-10-15 16:14:35 -0500

    initial commit

---
+++ mod.cpp
@@ -0,0 +1,8 @@
+name = "Foul Creatures Mod";
+picture = "foul_creatures\data\icons.paa";//THESE ARE PLACEHOLDERS
+logo = "foul_creatures\data\icons.paa";
+logoOver = "foul_creatures\data\icons.paa";
+tooltip = "Foul Creatures";
+tooltipOwned = "Foul Creatures";
+overview = "Adds spooky creatures for arma 3, can be used as warhammer daemons or just other general creatures as you feel like it";
+author = "LilyRose";
